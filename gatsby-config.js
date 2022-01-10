089509a39fe37ef48586bdf284a004aed0ec2125
Jorge <jorge.silva6@pt.bosch.com>
2022-01-10 23:45:12 +0000

    Updated social network

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     social: {
-      twitter: `jorgeadsilva`,
+      linkedin: `jorge16silva`,
     },
   },
   plugins: [
